1a1403c8f9f8a40776bb9faa63f22c4e62814845
BugHunter-X <askzenai@gmail.com>
2025-01-22 17:10:18 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,9 @@
+```javascript
+// Solution
+<div class="bg-gray-100 p-4">
+  <div class="flex">
+    <div class="w-1/2 bg-blue-500">Div 1</div>
+    <div class="w-1/2 bg-red-500">Div 2</div>
+  </div>
+</div>
+```
